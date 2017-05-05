e0cd4738578566a0a8ffa3b309675d66f7ce2189
zetavg <mail@zeta.vg>
2017-05-05 19:35:37 +0800

    setup babel-plugin-module-resolver and move code into app/

---
+++ app/index.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+import WelcomeView from 'views/WelcomeView'
+
+export default class App extends Component {
+  render() {
+    return (
+      <WelcomeView />
+    )
+  }
+}
